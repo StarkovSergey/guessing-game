be671a5945ac0d215e9af384223ed0563b55c02d
Starkov Sergey <starkovsr@gmail.com>
2021-09-05 00:37:19 +0300

    feat: finish the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.minNumber = min;
+      this.maxNumber = max;
     }
 
     guess() {
-
+      return Math.ceil((this.maxNumber + this.minNumber) / 2);
     }
 
     lower() {
-
+      this.maxNumber = this.guess();
     }
 
     greater() {
-
+      this.minNumber = this.guess();
     }
 }
 
